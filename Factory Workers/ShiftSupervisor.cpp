2fb734db79022ceb78babd06ca5037640bc1a489
emsample <esamplebox@gmail.com>
2026-05-02 16:15:59 -0400

    Finished the mutators, accessors and print function

--- Factory Workers/ShiftSupervisor.cpp
+++ Factory Workers/ShiftSupervisor.cpp
@@ -19,3 +19,42 @@ ShiftSupervisor::ShiftSupervisor(string n, int num, string date, double salary, 
     annualSalary = salary;
     annualBonus = bonus;
 }
+
+// PRE: salary >= 0
+// POST: annualSalary updated
+void ShiftSupervisor::setAnnualSalary(double salary)
+{
+    annualSalary = salary;
+}
+
+// PRE: bonus >= 0
+// POST: annualBonus updated
+void ShiftSupervisor::setAnnualBonus(double bonus)
+{
+    annualBonus = bonus;
+}
+
+// PRE: none
+// POST: returns salary
+double ShiftSupervisor::getAnnualSalary() const
+{
+    return annualSalary;
+}
+
+// PRE: none
+// POST: returns bonus
+double ShiftSupervisor::getAnnualBonus() const
+{
+    return annualBonus;
+}
+
+// PRE: none
+// POST: prints employee + supervisor data
+void ShiftSupervisor::printShiftSupervisor() const
+{
+    printEmployee();
+
+    cout << fixed << setprecision(2);
+    cout << "Annual Salary: $" << annualSalary << endl;
+    cout << "Annual Bonus: $" << annualBonus << endl;
+}
