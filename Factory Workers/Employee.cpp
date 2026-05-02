1f213f12f481bf0ca242d2725afddb40f923401d
emsample <esamplebox@gmail.com>
2026-05-02 15:02:05 -0400

    Okay just gonna do the comments now because its easier. Mutators added, nothing fancy.

--- Factory Workers/Employee.cpp
+++ Factory Workers/Employee.cpp
@@ -14,3 +14,25 @@ Employee::Employee(string n, int num, string date)
     employeeNumber = num;
     hireDate = date;
 }
+
+// PRE: none
+// POST: name updated
+void Employee::setName(string n)
+{
+    name = n;
+}
+
+// PRE: num >= 0
+// POST: employeeNumber updated
+void Employee::setEmployeeNumber(int num)
+{
+    employeeNumber = num;
+}
+
+// PRE: none
+// POST: hireDate updated
+void Employee::setHireDate(string date)
+{
+    hireDate = date;
+}
+
