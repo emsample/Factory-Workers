f52d1f0f9e4f001ead0cde561fc9b4cd354c6a6b
emsample <esamplebox@gmail.com>
2026-05-03 15:55:05 -0400

    I feel like I've really only made a few lines of code and have just swapped names out because all the classes are basically the same and all the tests are mostly the same.

--- Factory Workers/main.cpp
+++ Factory Workers/main.cpp
@@ -26,4 +26,26 @@ int main()
     e.setHireDate("12/31/2023");
     cout << "OUTPUT: " << endl;
     e.printEmployee();
+
+    cout << endl << "PRODUCTIONWORKER CLASS TESTS - DAY SHIFT" << endl;
+    ProductionWorker dayWorker("Maria Lopez", 2001, "03/12/2021", 1, 22.50);
+    cout << "Testing printProductionWorker(), which calls printEmployee():" << endl;
+    dayWorker.printProductionWorker();
+
+    cout << endl << "PRODUCTIONWORKER CLASS TESTS - NIGHT SHIFT" << endl;
+    ProductionWorker nightWorker("David Brown", 2002, "07/08/2020", 2, 25.75);
+    cout << "Testing printProductionWorker(), which calls printEmployee():" << endl;
+    nightWorker.printProductionWorker();
+
+    cout << endl << "SHIFTSUPERVISOR CLASS TESTS" << endl;
+    ShiftSupervisor supervisor("Linda Johnson", 3001, "11/02/2019", 62500.00, 4500.00);
+    cout << "Testing printShiftSupervisor(), which calls printEmployee():" << endl;
+    supervisor.printShiftSupervisor();
+    
+    cout << endl << "TEAMLEADER CLASS TESTS" << endl;
+    TeamLeader leader("Robert Davis", 4001, "05/20/2018", 1, 28.75, 850.00, 40, 36);
+    cout << "Testing printTeamLeader(), which calls printProductionWorker():" << endl;
+    leader.printTeamLeader();
+   
+    return 0;
 }
